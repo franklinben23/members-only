a8baf17853fbdd140a0a39555bc3bc9b2bc4af5d
franklin <franklinbenjamin032@gmail.com>
2021-03-19 19:36:03 -0400

    linter fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,5 @@
 module PostsHelper
   def show_name(post)
-    if user_signed_in?
-      content_tag(:p, "Author name: #{post.user.name}")
-    end
+    content_tag(:p, "Author name: #{post.user.name}") if user_signed_in?
   end
 end
