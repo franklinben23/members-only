868d9bc570969952af40c73bff0d8409beb2b429
franklin <franklinbenjamin032@gmail.com>
2021-03-20 13:22:40 -0400

    take logic out of views, add validation to post model

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
   def show_name(post)
     content_tag(:p, "Author name: #{post.user.name}") if user_signed_in?
   end
+
+  def show_links
+    if user_signed_in?
+      render 'down'
+    else
+      render 'up'
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates_length_of :title, maximum: 25
+  validates_length_of :body, maximum: 75
+  validates :body, presence: true
 end
